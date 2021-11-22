bb38b974905bc79469a392f1ecfacd57397aa407
Wend Carlos <wendcarlos18@hotmail.com>
2021-11-22 11:31:12 -0300

    Creating User Controller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,25 @@
+/* O controller é onde as informações vão entrar na aplicação. O controller vai manipular essas informações fazer algumas validações e depois chamar o Model pra gravar as informações no banco de dados */
+
+import { response } from "express"
+import { v4 } from "uuid"
+
+import User from '../models/User'
+
+class UserController{
+    async store(request, response){
+        // O metodo store vai cadastrar o novo usuario
+        const { name, email, password_hash, admin } = request.body
+
+        const user = await User.create({
+            id: v4(),
+            name,
+            email,
+            password_hash,
+            admin
+        })
+
+        return response.status(201).json({id: user.id, name, email, admin})
+    }
+}
+
+export default new UserController()
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,7 @@
 import { Router } from "express"
-import { v4 } from "uuid"
-
-import User from './app/models/User'
-
+import UserController from "./app/controllers/UserController"
 const routes = new Router()
 
-routes.get('/', async (request, response) => {
-    const user = await User.create({
-        id: v4(),
-        name: 'Wend Carlos',
-        email: 'wendcarlos@hotmail.com',
-        password_hash: 'def321456',
-        admin: true
-    })
-    return response.json(user)
-})
+routes.post('/users', UserController.store)
 
 export default routes
