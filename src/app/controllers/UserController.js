453fa47ee63e98343a252ef88a88b552b4f6dadd
Wend Carlos <wendcarlos18@hotmail.com>
2021-11-30 11:21:47 -0300

    Validating data with Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,12 +2,33 @@
 
 import { response } from "express"
 import { v4 } from "uuid"
+import * as Yup from "yup" // Yup valida os dados. O * representa todos os arquivos que devem ser exportados do Yup
 
 import User from '../models/User'
 
 class UserController{
     async store(request, response){
         // O metodo store vai cadastrar o novo usuario
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean()
+        })
+
+        /* if(!(await schema.isValid(request.body))){
+            return response
+                .status(400)
+                .json({error: "Make sure your data is correct"})
+        } */
+
+        try{
+            await schema.validateSync(request.body, {abortEarly: false})
+        }catch(err){
+            return response.status(400).json({error: err.errors})
+        }
+
         const { name, email, password_hash, admin } = request.body
 
         const user = await User.create({
