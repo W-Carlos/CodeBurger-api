cbd0a46b428198ca8734dc08a25f98dfecceb9bd
Wend Carlos <wendcarlos18@hotmail.com>
2021-12-10 11:42:40 -0300

    Creating Product Controller

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import ProductController from "./app/controllers/ProductController"
 import SessionController from "./app/controllers/SessionController"
 
 import UserController from "./app/controllers/UserController"
@@ -7,5 +8,6 @@ const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
+routes.post('/products', ProductController.store)
 
 export default routes
