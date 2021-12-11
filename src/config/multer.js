4deae0105322effcb746afe50503626498992899
Wend Carlos <wendcarlos18@hotmail.com>
2021-12-11 15:33:39 -0300

    Uploading images with Multer. I installed and configured multer, created the multer.js file and the upload folder.

---
+++ src/config/multer.js
@@ -0,0 +1,18 @@
+/* Configurações do multer. Upload de imagens.
+Obs: Não se armazena imagens em banco de dados, 
+o melhor a ser feito é armazenar em uma CDN. Como está 
+aplicação é pequena as imagens serão armazenadas numa pasta.
+*/
+
+import multer from 'multer'
+import { v4 } from 'uuid'
+import { extname, resolve } from 'path'
+
+export default {
+    storage: multer.diskStorage({
+        destination: resolve(__dirname, '..', '..', 'uploads'),
+        filename: (request, file, callback) => {
+            return callback(null, v4() + extname(file.originalname))
+        }
+    })
+}
