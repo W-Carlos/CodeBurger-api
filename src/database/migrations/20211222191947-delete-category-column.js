e702c57d44a37b387992405f5e43e3fa71ff237b
Wend Carlos <wendcarlos18@hotmail.com>
2021-12-22 16:51:28 -0300

    Creating and deleting fields from a table. I made the relationship between the categories and products table. I created migration: delete-category-column.js and create-category-column.js

---
+++ src/database/migrations/20211222191947-delete-category-column.js
@@ -0,0 +1,20 @@
+/* Migration para deleter coluna de categoria */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('products', 'category');
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.createColumn('products', {
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+     
+  }
+};
