6d461680c590ad451f1ab66eb60a1f6d9d4a07f4
Wend Carlos <wendcarlos18@hotmail.com>
2021-12-25 11:39:27 -0300

    Creating connection with MongoDB. I installed mongod in the mongo container. I installed the mongoose library. I made the connection between mongo and the application.

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 // Criando conexão do model com o banco de dados
 
 import Sequelize from 'sequelize'
+import mongoose from 'mongoose'
 
 import Product from '../app/models/Product'
 import User from '../app/models/User'
@@ -13,13 +14,26 @@ const models = [User, Product, Category]
 class Database {
     constructor(){
         this.init()
+        this.mongo()
     }
 
+    // Conexão com o postgres
     init(){
         this.connection = new Sequelize(configDatabase)
         models
             .map((model) => model.init(this.connection))
             .map((model) => model.associate && model.associate(this.connection.models))
+    }
+
+    // Conexão com o mongodb
+    mongo(){
+        this.mongoConnection = mongoose.connect(
+            'mongodb://localhost:27017/codeburger',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        )
     }
 }
 
