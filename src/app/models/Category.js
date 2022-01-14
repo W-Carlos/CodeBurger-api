9a270d9fec14228880400db5a028fe457b4ab8c5
Wend Carlos <wendcarlos18@hotmail.com>
2022-01-14 15:20:23 -0300

    I added the path and url field in the model and category.

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -5,7 +5,16 @@ import Sequelize, {Model} from 'sequelize'
 class Category extends Model {
     static init(sequelize){
         super.init({
-            name: Sequelize.STRING
+            name: Sequelize.STRING,
+            path: Sequelize.STRING,
+            url: {
+                // Este campo não existe no banco de dados
+                // Gerando uma url quando o usuario solicita informações do produto
+                type: Sequelize.VIRTUAL,
+                get(){
+                    return `http://localhost:3000/category-file/${this.path}`
+                }
+            }
         },
         {
             sequelize,
