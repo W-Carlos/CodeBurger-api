48180215240e1cbf17208dab5e758ce6dc87cb9f
Wend Carlos <wendcarlos18@hotmail.com>
2022-01-14 15:19:30 -0300

    I added the path and url field in the model and category.

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -28,6 +28,8 @@ class CategoryController{
 
                 const { name } = request.body
 
+                const { filename: path } = request.file
+
                 // Validando categoria repetida
 
                 const categoryExists = await Category.findOne({
@@ -36,14 +38,13 @@ class CategoryController{
                     }
                 })
 
-                console.log(categoryExists)
 
                 if(categoryExists){
                     return response.status(400).json({error: 'Category already exists'})
                 }
 
                 // Criando nova categoria
-                const { id } = await Category.create({ name })
+                const { id } = await Category.create({ name, path })
 
                 return response.json({name, id})
             }catch(err){
@@ -56,6 +57,51 @@ class CategoryController{
         const category = await Category.findAll()
 
         return response.json(category)
+    }
+
+    async update(request, response){
+            try{ const schema = Yup.object().shape({
+                    name: Yup.string()
+                })
+
+                try{
+                    //  Teste para verificar as informações e retorna o erro
+                    await schema.validateSync(request.body, {abortEarly: false})
+                }catch(err){
+                    return response.status(400).json({error: err.errors})
+                }
+
+                // Definido que só administradores podem criar uma nova categoria.
+                // Verificando se o usuario é admin
+                const {admin: isAdmin} = await User.findByPk(request.userId)
+
+                if(!isAdmin){
+                    return response.status(401).json()
+                }
+
+                const { name } = request.body
+
+                const { id } = request.params
+
+                // Verificando se categoria existe
+                const category = await Category.findByPk(id)
+
+                if(!category){
+                    return response.status(401).json({error: "Make sure your category id is correct"})
+                }
+
+                let path
+                if(request.file){
+                    path = request.file.filename
+                }
+
+                // Atualizando categoria
+                await Category.update({ name, path }, { where: { id } })
+
+                return response.status(200).json()
+            }catch(err){
+                console.log(err)
+            }
     }
 }
 
