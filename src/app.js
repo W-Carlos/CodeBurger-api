c0bef25e96cb8cff61f392afcc01132c2d209e4d
Wend Carlos <wendcarlos18@hotmail.com>
2022-02-08 11:21:20 -0300

    I installed cors and changed the server to port 3001

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-import express from "express"
+
+import express from 'express'
 import routes from "./routes"
 import { resolve } from "path"
+import cors from "cors"
 
 import './database'
 class App {
     constructor(){
-        this.app = 
-        express ()
+        this.app = express ()
+        this.app.use(cors())
 
         this.middlewares()
-        this.routes()
+        this.routes() 
     }
 
     middlewares(){
