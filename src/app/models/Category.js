abd2862430c21a31a1892fc3e53a8a728ec7676e
Wend Carlos <wendcarlos18@hotmail.com>
2022-05-22 20:50:33 -0300

    I changed the url port

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -13,7 +13,7 @@ class Category extends Model {
                     // Gerando uma url quando o usuario solicita informações do produto
                     type: Sequelize.VIRTUAL,
                     get() {
-                        return `http://localhost:3000/category-file/${this.path}`
+                        return `http://localhost:3001/category-file/${this.path}`
                     }
                 }
             },
